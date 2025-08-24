b45a70d2c407e8e461893384b3d3fda1daef0233
Hteik420 <121442932+Hteik420@users.noreply.github.com>
2025-08-24 16:04:35 +0800

    Add files via upload

---
+++ tests/gold_quality_checks.sql
@@ -0,0 +1,11 @@
+USE DataWarehouse
+-- Check the gender issue
+SELECT DISTINCT gender FROM gold.dim_customers
+
+SELECT * FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL
+
